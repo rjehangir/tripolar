c5328b17e8b4ae2575a6e4a76a4fb1330983c4d2
Alan Nise <github@alannise.com>
2015-05-12 19:28:18 -0700

    Created tripolar.cpp
    This is a first step to switching the project from C to C++.
    I need to check other files in, but will do this minimal amount now.
    
    THIS IS A NON WORKINNG CHECK-IN

---
+++ tripolar/tripolar.cpp
@@ -0,0 +1,17 @@
+/*
+ * tripolar.cpp
+ *
+ * Created: 5/12/2015 4:02:05 PM
+ *  Author: Alan
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
