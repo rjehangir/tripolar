93f85ae4bfa7832048b2fc0d27e47eb9c899cb9b
Alan Nise <github@alannise.com>
2015-05-28 11:30:02 -0700

    millis -> Used AVR's macro rather than magic number for TIMSK

--- src/millis.h
+++ src/millis.h
@@ -7,7 +7,7 @@
 		{
 			TCCR0 = 0x03; //Init Timer0, normal, prescalar = 64 (250Khz Clock Rate)
 			TCNT0 = 0x05; //Count up from 5 which makes the timer expire in 250 cycles.
-			TIMSK |= 0x01; //Set TOIE bit
+			TIMSK |= _BV(TOIE0); //Set TOIE bit
 		}		
 		inline uint32_t millis(void){return _ms;}		
 #endif
